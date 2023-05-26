52a7601dadc9f6deee25b3942e27b54658b50931
csobolew <47485699+csobolew@users.noreply.github.com>
2023-05-26 19:45:18 -0400

    :wrench: Basic layout, light/dark mode

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,49 @@
-import { MantineProvider, Text } from '@mantine/core';
+import { MantineProvider, ColorSchemeProvider, ColorScheme, Text } from '@mantine/core';
+import { useState } from 'react';
+import { AppShell, ActionIcon, Navbar, Header, Footer } from '@mantine/core';
+import { IconSun, IconMoonStars } from '@tabler/icons-react';
 
 export default function App() {
+  const [colorScheme, setColorScheme] = useState<ColorScheme>('light');
+  const toggleColorScheme = (value?: ColorScheme) =>
+    setColorScheme(value || (colorScheme === 'dark' ? 'light' : 'dark'));
+  const dark = colorScheme === 'dark';
   return (
-    <MantineProvider withGlobalStyles withNormalizeCSS>
-      <Text>Welcome to Mantine!</Text>
-    </MantineProvider>
+    <ColorSchemeProvider colorScheme={colorScheme} toggleColorScheme={toggleColorScheme}>
+      <MantineProvider theme={{ colorScheme }} withGlobalStyles withNormalizeCSS>
+        
+        <AppShell
+        padding="md"
+        header={<Header height={60} p="md">
+          
+          <div style={{ display: "flex" }}>
+            <Text
+              variant="gradient"
+              gradient={{ from: 'indigo', to: 'cyan', deg: 45}}
+              sx={{ fontFamily: 'Greycliff CF, sans-serif' }}
+              ta="left"
+              fz="xl"
+              fw={700}
+            >Carson Sobolewski</Text>          
+            <ActionIcon
+              variant="outline"
+              color={dark ? 'yellow' : 'blue'}
+              onClick={() => toggleColorScheme()}
+              title="Toggle color scheme"
+              style={{ marginLeft: "auto"}}
+            >
+              {dark ? <IconSun size="1.1rem" /> : <IconMoonStars size="1.1rem" />}
+            </ActionIcon>
+          </div>
+          </Header>}
+        styles={(theme) => ({
+          main: { backgroundColor: theme.colorScheme === 'dark' ? theme.colors.dark[8] : theme.colors.gray[0] },
+        })}
+        >
+
+        </AppShell>
+        
+      </MantineProvider>
+    </ColorSchemeProvider>
   );
 }
