af721c39f3e0aaeaea9065ea76d2584b66ca9386
csobolew <47485699+csobolew@users.noreply.github.com>
2023-06-08 01:30:50 -0400

    Complete Navbar :wrench:

--- src/Components/Header.tsx
+++ src/Components/Header.tsx
@@ -1,39 +1,66 @@
-import { Badge, Button, Text, ActionIcon, ColorSchemeProvider, ColorScheme } from '@mantine/core'
-import { IconSun, IconMoonStars } from '@tabler/icons-react';
-import React from 'react'
-import { useState } from 'react';
+import { Text, ActionIcon, ColorScheme } from "@mantine/core";
+import { IconSun, IconMoonStars } from "@tabler/icons-react";
+import { HelmetProvider, Helmet } from "react-helmet-async";
+import { Link } from "react-scroll";
 
 interface HeaderProps {
     dark: Boolean;
     colorScheme: ColorScheme;
-    toggleColorScheme: (value?:ColorScheme) => void;
+    toggleColorScheme: (value?: ColorScheme) => void;
 }
 
-const Header = (props : HeaderProps) => {
-    return(
+const Header = (props: HeaderProps) => {
+    return (
         <header>
+            <HelmetProvider>
+                <Helmet>
+                    <meta
+                        name="theme-color"
+                        content={props.dark ? "#1a1b1e" : "#ffffff"}
+                    />
+                </Helmet>
+            </HelmetProvider>
+
             <div className="content-title">
                 <div style={{ display: "flex" }}>
-                <Text
-                variant="gradient"
-                gradient={props.dark ? {from: 'yellow', to: 'yellow', deg: 45} : {from: 'cyan', to:'cyan', deg: 45}}
-                sx={{ fontFamily: 'Greycliff CF, sans-serif' }}
-                ta="left"
-                fz="xl"
-                fw={700}
-                >Carson Sobolewski</Text>  
+                    <Text
+                        variant="gradient"
+                        gradient={
+                            props.dark
+                                ? { from: "yellow", to: "yellow", deg: 45 }
+                                : { from: "cyan", to: "cyan", deg: 45 }
+                        }
+                        sx={{ fontFamily: "Greycliff CF, sans-serif" }}
+                        ta="left"
+                        fz="xl"
+                        fw={700}
+                    >
+                        Carson Sobolewski
+                    </Text>
                 </div>
                 <div className="navbar">
                     <div className="navbar-item">
+                        <Link to="projects">Projects</Link>
+                    </div>
+                    <div className="navbar-item">
+                        <Link to="research">Research</Link>
+                    </div>
+                    <div className="navbar-item">
+                        <Link to="about">About Me</Link>
+                    </div>
+                    <div className="navbar-item">
                         <ActionIcon
-                        variant="outline"
-                        color={props.dark ? 'yellow' : 'cyan'}
-                        onClick={() => props.toggleColorScheme()}
-                        title="Toggle Color Scheme"
-                        style={{ marginLeft: "auto"
-                        }}
+                            variant="outline"
+                            color={props.dark ? "yellow" : "cyan"}
+                            onClick={() => props.toggleColorScheme()}
+                            title="Toggle Color Scheme"
+                            style={{ marginLeft: "auto" }}
                         >
-                        {props.dark ? <IconSun size="1.1rem" /> : <IconMoonStars size="1.1rem" />}
+                            {props.dark ? (
+                                <IconSun size="1.1rem" />
+                            ) : (
+                                <IconMoonStars size="1.1rem" />
+                            )}
                         </ActionIcon>
                     </div>
                 </div>
